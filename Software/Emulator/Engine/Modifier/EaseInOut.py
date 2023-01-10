462f3dfd53053ebbd0ec72356644723a8cc71b07
Florian Baumgartner <florian.b-h@bluewin.ch>
2023-01-10 20:35:50 +0100

    Completed Ease-Functions

--- Software/Emulator/Engine/Modifier/EaseInOut.py
+++ Software/Emulator/Engine/Modifier/EaseInOut.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("..")
 from Modules import Modifier
 
-class EaseIn(Modifier):
+class EaseInOut(Modifier):
     def __init__(self, id):
         super().__init__(id)
         self.parameterInputs.append({"name": "input", "module": None, "sourceIndex" : 0})
@@ -24,25 +24,25 @@ class EaseIn(Modifier):
             output = 0.0
         else:
             if easeType == 0:       # Sine
-                output = 1.0 - np.cos((x * np.pi) / 2.0)
+                output = -(np.cos(np.pi * x) - 1.0) / 2.0
             elif easeType == 1:     # Quad
-                output = x**2
+                output = 2.0 * x**2 if(x < 0.5) else x * (4.0 - 2.0 * x) - 1.0
             elif easeType == 2:     # Cubic
-                output = x**3
+                output = 4.0 * x**3 if(x < 0.5) else 1.0 - (-2.0 * x + 2.0)**3 / 2.0
             elif easeType == 3:     # Quart
-                output = x**4
+                output = 8.0 * x**4 if(x < 0.5) else 1.0 - (-2.0 * x + 2.0)**4 / 2.0
             elif easeType == 4:     # Quint
-                output = x**5
+                output = 16.0 * x**5 if(x < 0.5) else 1.0 - (-2.0 * x + 2.0)**5 / 2.0
             elif easeType == 5:     # Expo
-                output = 2.0**(10.0 * x - 10.0)
+                output = 2.0**(20.0 * x - 10.0) / 2.0 if(x < 0.5) else (2.0 - 2.0**(-20.0 * x + 10.0)) / 2.0
             elif easeType == 6:     # Circ
-                output = 1.0 - np.sqrt(1.0 - x**2)
+                output = (1.0 - np.sqrt(1.0 - (2.0 * x)**2)) / 2.0 if(x < 0.5) else (np.sqrt(1.0 - (-2.0 * x + 2.0)**2) + 1.0) / 2.0
             elif easeType == 7:     # Back
-                output = 2.70158 * x**3 - 1.70158 * x**2
+                output = ((2.0 * x)**2 * (7.1898 * x - 2.5949)) / 2.0 if(x < 0.5) else ((2.0 * x - 2.0)**2 * (3.5949 * (x * 2.0 - 2.0) + 2.5949) + 2) / 2.0
             elif easeType == 8:     # Elastic
-                output = x**4 * np.sin(x * np.pi * 4.586) * 1.0377                              # Original: 4.5
+                output = -(2.0**(20.0 * x - 10.0) * np.sin((20.0 * x - 11.125) * ((2.0 * np.pi) / 4.5))) / 2.0 if(x < 0.5) else (2.0**(-20.0 * x + 10.0) * np.sin((20.0 * x - 11.125) * ((2.0 * np.pi) / 4.5))) / 2.0 + 1.0
             elif easeType == 9:     # Bounce
-                output = 2.0**(6.0 * (x - 1.0)) * np.abs(np.sin(x * np.pi * 3.612)) * 1.065     # Original: 3.5
+                output = 8.0 * 2.0**(8.0 * (x - 1.0)) * np.abs(np.sin(x * np.pi * 7.0)) if(x < 0.5) else 1.0 - 8.0 * 2.0**(-8.0 * x) * np.abs(np.sin(x * np.pi * 7.0))
             else:
                 raise Exception("Easy-Function type out of bound!")
             
@@ -70,16 +70,16 @@ if __name__ == '__main__':
     ramp.setParameterInput(3, Coefficient(6, stop))
     ramp.setParameterInput(4, Coefficient(7, phase))
     
-    easeIn = EaseIn(1)
-    easeIn.setParameterInput(0, ramp)
-    easeIn.setParameterInput(1, Coefficient(8, easyType))
+    easeInOut = EaseInOut(1)
+    easeInOut.setParameterInput(0, ramp)
+    easeInOut.setParameterInput(1, Coefficient(8, easyType))
     
     plotter = Analyzer.ParameterPlotter(2, autoMove=False, standalone=True)
-    plotter.setParameterInput(0, easeIn, 0)
+    plotter.setParameterInput(0, easeInOut, 0)
     
     def update(t):
         ramp.update(t)
-        easeIn.update(t)
+        easeInOut.update(t)
         plotter.update(t)
 
     plotter.updateFunction = update
